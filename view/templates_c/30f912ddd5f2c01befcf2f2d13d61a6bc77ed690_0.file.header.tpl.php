d4799419065707238c0f999e28f057a1b09a484e
Allan <ahovenga@gmail.com>
2023-08-08 18:56:12 -0600

    update

---
+++ view/templates_c/30f912ddd5f2c01befcf2f2d13d61a6bc77ed690_0.file.header.tpl.php
@@ -0,0 +1,45 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-08-09 00:44:56
+  from 'C:\wamp64\www\progra3_Login_Registro\view\templates\header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_64d2e188d0e998_18112310',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '30f912ddd5f2c01befcf2f2d13d61a6bc77ed690' => 
+    array (
+      0 => 'C:\\wamp64\\www\\progra3_Login_Registro\\view\\templates\\header.tpl',
+      1 => 1688607460,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64d2e188d0e998_18112310 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+    <head>
+        <title><?php echo $_smarty_tpl->tpl_vars['titulo']->value;?>
+</title>
+        
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+        <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+
+
+    </head>
+    
+    <body><?php }
+}
---
+++ view/templates_c/5624fab74a7f77e34b639eca3d3843e3b11afadc_0.file.footer.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-08-09 00:44:56
+  from 'C:\wamp64\www\progra3_Login_Registro\view\templates\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_64d2e188df8885_61095896',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5624fab74a7f77e34b639eca3d3843e3b11afadc' => 
+    array (
+      0 => 'C:\\wamp64\\www\\progra3_Login_Registro\\view\\templates\\footer.tpl',
+      1 => 1687395958,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64d2e188df8885_61095896 (Smarty_Internal_Template $_smarty_tpl) {
+?>    </body>
+</html>
+<?php }
+}
